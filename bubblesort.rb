a72f92583a78019c8da7f563f4a7633cab6e9640
Daniel Santander <danyvo1d@gmail.com>
2020-03-09 11:36:17 -0600

    Fixed puts ending line issue

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,10 +1,8 @@
-bubble_array = [98, 7, 52, 9, 3, 24, 0, 16]
-
 def bubble_sort(list)
   return list if list.size <= 1
 
   swapped = true
-  while swapped {
+  while swapped do
     swapped = false
     0.upto(list.size - 2) do |i|
       if list[i] > list[i + 1]
@@ -12,27 +10,31 @@ def bubble_sort(list)
         swapped = true
       end
     end
-  }
+  end
   list
 end
 
-def bubble_sort_by (array)
+
+def bubble_sort_by(array)
   return array if array.size <= 1
 
-  len = array.length - 1 # from the end of the array
-  len.step(1, -1) { |a| # step back
-    (0...a).each do |i| # and to each
-      test = yield(array[i], array[i + 1]) # combination (array[i] = left, array[i+1] = right)
-      next unless test.positive? # from end to start, n, n-1 to 0
-      temp = array[i] # [temp] = [left]
-      array[i] = array[i + 1] # [left] = [right]
-      array[i + 1] = temp # a [right] = [temp]
+  len = array.length - 1 
+  len.step(1, -1) { |a| 
+    (0...a).each do |i| 
+      test = yield(array[i], array[i + 1]) 
+      next unless test.positive? 
+      temp = array[i] 
+      array[i] = array[i + 1] 
+      array[i + 1] = temp
     end
   }
   array
 end
 
+bubble_array = [98, 7, 52, 9, 3, 24, 0, 16]
+array = ["hi", "hello", "hey"]
 
-puts bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  left.length - right.length
-end
+puts "#{bubble_sort(bubble_array)}"
+puts "#{bubble_sort_by(["hi","hello","hey"]) do |left,right|
+     left.length - right.length
+end}"
