9974a54877358f5e4c28d0508457b99990def0e6
Daniel Santander <danyvo1d@gmail.com>
2020-03-09 12:53:04 -0600

    Fixed rubocop offenses

--- bubblesort.rb
+++ bubblesort.rb
@@ -2,7 +2,7 @@ def bubble_sort(list)
   return list if list.size <= 1
 
   swapped = true
-  while swapped do
+  while swapped
     swapped = false
     0.upto(list.size - 2) do |i|
       if list[i] > list[i + 1]
@@ -14,27 +14,27 @@ def bubble_sort(list)
   list
 end
 
-
 def bubble_sort_by(array)
   return array if array.size <= 1
 
-  len = array.length - 1 
-  len.step(1, -1) { |a| 
-    (0...a).each do |i| 
-      test = yield(array[i], array[i + 1]) 
-      next unless test.positive? 
-      temp = array[i] 
-      array[i] = array[i + 1] 
+  len = array.length - 1
+  len.step(1, -1) do |a|
+    (0...a).each do |i|
+      test = yield(array[i], array[i + 1])
+      next unless test.positive?
+
+      temp = array[i]
+      array[i] = array[i + 1]
       array[i + 1] = temp
     end
-  }
+  end
   array
 end
 
 bubble_array = [98, 7, 52, 9, 3, 24, 0, 16]
-array = ["hi", "hello", "hey"]
+array = %w[hi hello hey]
 
-puts "#{bubble_sort(bubble_array)}"
-puts "#{bubble_sort_by(["hi","hello","hey"]) do |left,right|
-     left.length - right.length
-end}"
+print "#{bubble_sort(bubble_array)}\n"
+print "#{bubble_sort_by(array) do |left, right|
+  left.length - right.length
+end}\n"
