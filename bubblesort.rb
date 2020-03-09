f80c1812540795469c929715994a1cd8bb040144
Daniel Santander <danyvo1d@gmail.com>
2020-03-09 10:46:42 -0600

    Trying yield code

--- bubblesort.rb
+++ bubblesort.rb
@@ -19,14 +19,14 @@ end
 def bubble_sort_by (array)
   return array if array.size <= 1
 
-  swapped = true
-  while swapped {
-    swapped = false
-    0.upto(list.size - 2) do |i|
-      if list[i] > list[i + 1]
-        list[i], list[i + 1] = list[i + 1], list[i] if yield (array[i], array[i-1]).positive?
-        swapped = true
-      end
+  len = array.length - 1 # from the end of the array
+  len.step(1, -1) { |a| # step back
+    (0...a).each do |i| # and to each
+      test = yield(array[i], array[i + 1]) # combination (array[i] = left, array[i+1] = right)
+      next unless test.positive? # from end to start, n, n-1 to 0
+      temp = array[i] # [temp] = [left]
+      array[i] = array[i + 1] # [left] = [right]
+      array[i + 1] = temp # a [right] = [temp]
     end
   }
   array
