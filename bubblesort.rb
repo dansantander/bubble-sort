100d636ac4741cac978795c3329feec7025a2e32
Rodolfo Mata <rodmatrod@gmail.com>
2020-03-09 10:13:21 -0600

    Sort by block trial

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,17 +1,33 @@
 bubble_array = [98, 7, 52, 9, 3, 24, 0, 16]
 
 def bubble_sort(list)
-  return list if list.size <= 1 
+  return list if list.size <= 1
+
   swapped = true
-  while swapped do
+  while swapped {
     swapped = false
-    0.upto(list.size-2) do |i|
-      if list[i] > list[i+1]
-        list[i], list[i+1] = list[i+1], list[i] # swap values
+    0.upto(list.size - 2) do |i|
+      if list[i] > list[i + 1]
+        list[i], list[i + 1] = list[i + 1], list[i] # swap values
         swapped = true
       end
-    end    
-  end
-list
+    end
+  }
+  list
 end
-  puts bubble_sort(bubble_array)
+
+def bubble_sort_by (array)
+  return array if array.size <= 1
+
+  swapped = true
+  while swapped {
+    swapped = false
+    yield
+  }
+  array
+end
+
+
+puts bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
